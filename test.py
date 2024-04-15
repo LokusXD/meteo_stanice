2699162793c05b8fd3bc17fff74679739f13df67
LokusXD <jirkalokvenc@outlook.com>
2024-04-15 11:02:43 +0200

    xdxd

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
-x = 0.1 + 0.2
+from PyQt5 import QtWidgets
 
-print(x)
+app = QtWidgets.QApplication([])
+
+# QComboBox - políčko pro výběr z několika možností
+box = QtWidgets.QComboBox()
+box.addItem('First Option')
+box.addItem('Second Option')
+
+# Základní varianta napojí na activated[int]
+box.activated.connect(print)
+
+# Výběr varianty signálu pomocí hranatých závorek
+box.activated[str].connect(print)
+box.activated[int].connect(print)
+
+box.show()
+
+app.exec()
