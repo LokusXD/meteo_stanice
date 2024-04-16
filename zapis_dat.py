ad72d6a65d0a1e88557f3fa98d989945aa797607
LokusXD <jirkalokvenc@outlook.com>
2024-04-16 12:01:11 +0200

    xd

--- zapis_dat.py
+++ zapis_dat.py
@@ -1,14 +1,18 @@
 import json
 import random
 import time
+from PyQt5 import QtWidgets
+from matplotlib import pyplot as plt
 
 def json_fille():
     # Definice dat
     x = random.randint(1,10)
     y = random.randint(1,10)
+    t = random.randint(1,10)
     data = {
-        "length": x,   # Příklad délky
-        "angle": y     # Příklad úhlu
+        "temperature": x,   # Příklad délky
+        "humidity": y,     # Příklad úhlu
+        "time": t
     }
 
     # Název souboru, do kterého chcete data uložit
@@ -20,6 +24,15 @@ def json_fille():
 
     print("Data byla zapsána do JSON souboru.")
 
-while True:
-    json_fille()
-    time.sleep(10)
+#while True:
+#    json_fille()
+#    time.sleep(10)
+x = []    
+y = []    
+    
+plt.plot(x,y)    
+    
+plt.title("teplota a vlhkost")    
+plt.ylabel('Y axis')    
+plt.xlabel('X axis')    
+plt.show()    
